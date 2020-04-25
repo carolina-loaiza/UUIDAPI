85a9f2f2db90970b5d5aafa81bc57ade6fcd9cff
Carolina Loaiza <karolina801chan@gmail.com>
2020-04-24 21:09:14 -0600

    Added /create

---
+++ src/main/java/com/cenfotec/UUIDAPI/controllers/UUIDController.java
@@ -0,0 +1,14 @@
+package com.cenfotec.UUIDAPI.controllers;
+
+import java.util.UUID;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UUIDController {
+	@GetMapping("/create")
+	public ResponseEntity<UUID> getUUID(){
+		return ResponseEntity.ok(UUID.randomUUID());
+	}
+}
